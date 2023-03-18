a21568b947efebf87352bb29f4b2215effad606c
SaraG94 <sara.gherbassi.94@gmail.com>
2023-03-19 00:55:13 +0100

    START

---
+++ index.php
@@ -0,0 +1,5 @@
+<form action="result.php" method="GET">
+    <input type ="text" name="paragraph" placeholder="inserisci un paragrafo">
+    <input type ="text" name="censored" placeholder="inserisci parola da censurare">
+    <button type ="submit"> Invia </button>
+</form>
---
+++ result.php
@@ -0,0 +1,6 @@
+<?php 
+
+$text = $_GET['paragraph'];
+$word =$_GET['censored'];
+var_dump($_GET);
+?>
